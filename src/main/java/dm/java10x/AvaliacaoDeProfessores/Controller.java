3180db2c8c3590ad1b6c6acc2f022d063ffad36f
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-03-09 20:55:40 -0300

    Criado a controller

---
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/Controller.java
@@ -0,0 +1,15 @@
+package dm.java10x.AvaliacaoDeProfessores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa aplicacao";
+    }
+
+}
