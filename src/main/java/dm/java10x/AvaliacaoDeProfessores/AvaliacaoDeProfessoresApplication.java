143d0449c5220b78196fa4f4c55c764f4144a9b5
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-03-09 20:19:23 -0300

    first commit

---
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/AvaliacaoDeProfessoresApplication.java
@@ -0,0 +1,13 @@
+package dm.java10x.AvaliacaoDeProfessores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoDeProfessoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoDeProfessoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dm/java10x/AvaliacaoDeProfessores/AvaliacaoDeProfessoresApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.java10x.AvaliacaoDeProfessores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoDeProfessoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
