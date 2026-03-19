1c4d19169bd9242678d8b1284f49f9ebe171b53c
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-03-19 08:25:06 -0300

    Atualização das dependencias

--- src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/Materia/Professores/ProfessorController.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/Materia/Professores/ProfessorController.java
@@ -1,4 +1,4 @@
-package dm.java10x.AvaliacaoDeProfessores.Professores;
+package dm.java10x.AvaliacaoDeProfessores.Controler.Materia.Professores;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
