1f92d2d5cde2b09328a2dff3c8b9d5f9f4012259
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-04-03 15:51:30 -0300

    pequenos ajustes

--- src/main/java/dm/java10x/AvaliacaoDeProfessores/AvaliacaoDeProfessoresApplication.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/AvaliacaoDeProfessoresApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvaliacaoDeProfessoresApplication {
+public class  AvaliacaoDeProfessoresApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvaliacaoDeProfessoresApplication.class, args);
--- src/main/java/dm/java10x/AvaliacaoDeProfessores/repository/AvaliacaoRepository.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/repository/AvaliacaoRepository.java
@@ -1,9 +1,14 @@
 package dm.java10x.AvaliacaoDeProfessores.repository;
 
+import dm.java10x.AvaliacaoDeProfessores.model.AulaModel;
 import dm.java10x.AvaliacaoDeProfessores.model.AvaliacaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel, Long> {
+
+    void deleteByAulaModel(AulaModel aulaModel);
 }
