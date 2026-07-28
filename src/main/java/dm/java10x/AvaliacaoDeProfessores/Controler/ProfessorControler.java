57870083f577b3ca81c5df6beb1fad4b619c08d2
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-07-28 16:03:39 -0300

    Correção dos controllers

--- src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/ProfessorControler.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/ProfessorControler.java
@@ -1,25 +1,13 @@
 package dm.java10x.AvaliacaoDeProfessores.Controler;
 
-import dm.java10x.AvaliacaoDeProfessores.dto.ProfessorUpdateDTO;
-import dm.java10x.AvaliacaoDeProfessores.enumeradores.Adjetivo;
-import dm.java10x.AvaliacaoDeProfessores.enumeradores.Melhorias;
-import dm.java10x.AvaliacaoDeProfessores.model.abstracte.Image;
 import dm.java10x.AvaliacaoDeProfessores.model.entity.ProfessorModel;
-import dm.java10x.AvaliacaoDeProfessores.service.AlunoService;
 import dm.java10x.AvaliacaoDeProfessores.service.AulaService;
 import dm.java10x.AvaliacaoDeProfessores.service.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/professor")
@@ -49,17 +37,9 @@ public class ProfessorControler {
         return ResponseEntity.ok(media);
     }
 
-    @GetMapping("/adjetivo/{id}")
-    public ResponseEntity<?> buscarAdjetivoPorId(@PathVariable Long id){
-        aulaService.deletarAulasVencidas();
-        Map<Adjetivo, Integer> adjetivo = professorService.adjetivos(id);
-        return ResponseEntity.ok(adjetivo);
-    }
-
-    @GetMapping("/melhoria/{id}")
-    public ResponseEntity<?> buscarMelhoriaPorId(@PathVariable Long id){
-        aulaService.deletarAulasVencidas();
-        List<Melhorias> melhorias = professorService.melhorias(id);
-        return ResponseEntity.ok(melhorias);
+    @GetMapping("/comentario/{id}")
+    public ResponseEntity<List<String>> buscarComentarioPorId(@PathVariable Long id){
+        List<String> comentario = professorService.buscarComentariosPeloId(id);
+        return ResponseEntity.ok().body(comentario);
     }
 }
